4ca2646f001f097c2906759af5f633162ce08137
joan-pookie <joan92311@gmail.com>
2025-06-08 22:40:46 +0300

    Update fare.js

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,1 +1,24 @@
 
+function calculateBodaFare(distanceInKm) {
+  const baseFare = 50;
+  const chargePerKm = 15;
+
+  const totalFare = baseFare + (distanceInKm * chargePerKm);
+
+  console.log(Uko kwote? Io ni ${distanceInKm} km:);
+  console.log(Ukikalia Pikipiki: KES ${baseFare});
+  console.log(Mpaka Uko: KES ${distanceInKm * chargePerKm});
+  console.log(Total: KES ${totalFare});
+  console.log("\nPanda Pikipiki!");
+}
+
+// Prompt user for input
+const input = prompt("Unafika wapi Mkubwa? Kilometer ngapi?:");
+const distance = Number(input);
+
+// Validate and call the function
+if (!isNaN(distance) && distance > 0) {
+  calculateBodaFare(distance);
+} else {
+  console.log("Tafadhali andika nambari sahihi ya kilomita.");
+}
